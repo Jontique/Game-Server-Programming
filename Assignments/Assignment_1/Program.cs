ecc5460923558580d143c0815b132c3e560e22b8
Jontique <sentinelfin@gmail.com>
2018-08-22 15:23:10 +0300

    assignment 1 wip
    no implementation yet

---
+++ Assignments/Assignment_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        string _bikeStationName;
+
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            ICityBikeDataFetcher dataFetcher = new ICityBikeDataFetcher();
+            Console.WriteLine("Enter station's name: ");
+            _bikeStationName = Console.ReadLine();
+            dataFetcher.GetBikeCountInStation("string");
+        }
+    }
+
+    public interface ICityBikeDataFetcher
+    {
+        Task<int> GetBikeCountInStation(string stationName);
+    }
+
+    class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
+    {
+
+    }
+}
