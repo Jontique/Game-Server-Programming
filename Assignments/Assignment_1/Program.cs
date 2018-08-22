85cf2c1ff669b6e57c4b5528fd54bafc02422847
Jontique <joni.h94@hotmail.com>
2018-08-22 19:26:44 +0300

    Added objects and custom class
    TODO: get bike data in the right format, print it out, get amount of
    bikes

--- Assignments/Assignment_1/Program.cs
+++ Assignments/Assignment_1/Program.cs
@@ -1,28 +1,66 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft;
 
 namespace Assignment_1
 {
     class Program
     {
-        string _bikeStationName;
 
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello World!");
-            ICityBikeDataFetcher dataFetcher = new ICityBikeDataFetcher();
+            string _bikeStationName;
+            ICityBikeDataFetcher dataFetcher = new RealTimeCityBikeDataFetcher();
             Console.WriteLine("Enter station's name: ");
             _bikeStationName = Console.ReadLine();
-            dataFetcher.GetBikeCountInStation("string");
+            dataFetcher.GetBikeCountInStation(_bikeStationName).Wait();
+            Console.ReadKey();
+
         }
     }
 
     public interface ICityBikeDataFetcher
     {
-        Task<int> GetBikeCountInStation(string stationName);
+       Task<int> GetBikeCountInStation(string stationName);
     }
 
     class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
     {
+        public async Task<int> GetBikeCountInStation(string station)
+        {
+           // int amount;
+            var httpClient = new System.Net.Http.HttpClient();
+            //var test = await httpClient.GetAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
+            var toast = await httpClient.GetByteArrayAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
+            string test = System.Text.Encoding.UTF8.GetString(toast);
+            //toast.ToString();
+            
+            //Console.WriteLine("station: "  + station + ", test = " + test);
+           // test.Split("id");
+
+           
+
+           // string[] stations;
+
+            var stationInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<BikeStation>(test);
+            //stations = test.Split(',');
+
+            Console.WriteLine(stationInfo);
+
+
+            return 1;
+        }
+
+    }
+
+    class BikeStation
+    {
+        string name;
+        string state;
+        int id;
+        int bikesAvailable;
+        int spacesAvailable;
 
     }
 }
