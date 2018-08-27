01a5a51b86226cdff9dcaf48a5a1178fa48ae58b
Jontique <sentinelfin@gmail.com>
2018-08-27 15:38:11 +0300

    class list of objects
    TODO: edit objects to make them work

--- Assignments/Assignment_1/Program.cs
+++ Assignments/Assignment_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading;
 using Newtonsoft;
@@ -31,36 +32,49 @@ namespace Assignment_1
         {
            // int amount;
             var httpClient = new System.Net.Http.HttpClient();
+            int bikesAvailable;
             //var test = await httpClient.GetAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
             var toast = await httpClient.GetByteArrayAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
             string test = System.Text.Encoding.UTF8.GetString(toast);
             //toast.ToString();
             
+            //Console.Write(test);
             //Console.WriteLine("station: "  + station + ", test = " + test);
            // test.Split("id");
 
-           
+
+          // Console.Write(test);
 
            // string[] stations;
 
-            var stationInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<BikeStation>(test);
+            BikeRentalStationList stationInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<BikeRentalStationList>(test);
             //stations = test.Split(',');
 
-            Console.WriteLine(stationInfo);
+           // stationInfo.ToString();
+           foreach(object bikestation in stationInfo)
+           {
+               
+           }
+            Console.WriteLine(stationInfo.stations[5]);
 
+          /*  for(int i = 0; i < stationInfo.stations.Length; ++i)
+            {
+                if(stationInfo.stations[i].)
+            }
+*/
 
             return 1;
         }
 
     }
 
-    class BikeStation
+    public class BikeRentalStationList
     {
-        string name;
-        string state;
-        int id;
-        int bikesAvailable;
-        int spacesAvailable;
-
+        public object[] stations;
+      /*  public string name;
+        public string state;
+        public int id;
+        public int bikesAvailable;
+        public int spacesAvailable;*/
     }
 }
