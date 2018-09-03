74cb14b396095347d8d4b82b1404d631a4a0b297
Jontique <joni.h94@hotmail.com>
2018-09-03 19:28:07 +0300

    Add NotFoundException custom exception
    part 6 of the assignment was missing. fixed

--- Assignments/Assignment_1/Program.cs
+++ Assignments/Assignment_1/Program.cs
@@ -16,6 +16,8 @@ namespace Assignment_1
         public ICityBikeDataFetcher dataFetcher;
 
         string[] invalidCharacters = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+
+        
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -78,28 +80,35 @@ namespace Assignment_1
     {           
         
         public int _bikesAvailable;
+        
         public async Task<int> GetBikeCountInStation(string station)
         {
-           // int amount;
             var httpClient = new System.Net.Http.HttpClient();
 
             var toast = await httpClient.GetByteArrayAsync("http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental");
             string utfString = System.Text.Encoding.UTF8.GetString(toast);
             BikeRentalStationList stationInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<BikeRentalStationList>(utfString);
             
-            if(stationInfo.stations.Length != 0) 
+            try 
             {
-                for(int i = 0; i < stationInfo.stations.Length; ++i) 
+                if(stationInfo.stations.Length != 0) 
                 {
-                    if(station.ToLower() == stationInfo.stations[i].name.ToLower()) 
+                    for(int i = 0; i < stationInfo.stations.Length; ++i) 
                     {
-                        _bikesAvailable = stationInfo.stations[i].bikesAvailable;
-                        Console.WriteLine(stationInfo.stations[i].name + " has " + _bikesAvailable + " bike(s) available.\n");
-                        return _bikesAvailable;
+                        if(station.ToLower() == stationInfo.stations[i].name.ToLower()) 
+                        {
+                            _bikesAvailable = stationInfo.stations[i].bikesAvailable;
+                            Console.WriteLine(stationInfo.stations[i].name + " has " + _bikesAvailable + " bike(s) available.\n");
+                            return _bikesAvailable;
+                        }
                     }
                 }
+                throw new NotFoundException("Station with that name was not found");
             }
-            Console.WriteLine("Station with that name was not found");
+            catch(NotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             return 0;
         }
 
@@ -114,27 +123,36 @@ namespace Assignment_1
 
             using (StreamReader streamReader = new StreamReader(@"./bikedata.txt")) 
             {
+                try
+                {
 
-                while(!streamReader.EndOfStream) 
-                {  
-                    var line = await streamReader.ReadLineAsync();
-                    if(line.ToLower().Contains(station.ToLower())) 
-                    {
-
-                        string[] result = Regex.Split(line, @"\D+"); 
-                        foreach(string temp in result) 
+                
+                    while(!streamReader.EndOfStream) 
+                    {  
+                        var line = await streamReader.ReadLineAsync();
+                        if(line.ToLower().Contains(station.ToLower())) 
                         {
-                            if(string.IsNullOrEmpty(temp) == false)  //if regex doesn't find a number
-                            {
-                                int _bikesAvailable = int.Parse(temp);
-                                Console.WriteLine(station + " has "+ _bikesAvailable + " bikes available.\n");
-                                return _bikesAvailable;
-                            }
-                        }
 
+                            string[] result = Regex.Split(line, @"\D+"); 
+                            foreach(string temp in result) 
+                            {
+                                if(string.IsNullOrEmpty(temp) == false)  //if regex doesn't find a number
+                                {
+                                    int _bikesAvailable = int.Parse(temp);
+                                    Console.WriteLine(station + " has "+ _bikesAvailable + " bikes available.\n");
+                                    return _bikesAvailable;
+                                }
+                            }
+
+                        }
                     }
+                    throw new NotFoundException("Station with that name was not found");
                 }
-                Console.WriteLine("Station with that name was not found.\n");
+                catch(NotFoundException e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+
             }
 
             return 0;
@@ -155,6 +173,18 @@ namespace Assignment_1
         public int id;
         public int bikesAvailable;
         public int spacesAvailable;
+
+    }
+
+    [Serializable()]
+    public class NotFoundException : System.Exception 
+    {
+        public NotFoundException() : base() { }
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, System.Exception inner) : base(message, inner) 
+        {
+            Console.WriteLine(message);
+        }
 
     }
 
